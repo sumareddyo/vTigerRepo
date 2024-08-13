d8e1a2e9f81a7b64585e3b64c80bedb80a363d6b
Suma Reddy <Suma Reddy@SumaReddy>
2024-08-13 13:10:29 +0530

    committing contact package

---
+++ vTiger/src/test/java/com/vtiger/contact/CreateConWithSupportDate_Test.java
@@ -0,0 +1,12 @@
+package com.vtiger.contact;
+
+import org.testng.annotations.Test;
+
+public class CreateConWithSupportDate_Test {
+
+	@Test
+	public void test()
+	{
+		System.out.println("Created contact");
+	}
+}
